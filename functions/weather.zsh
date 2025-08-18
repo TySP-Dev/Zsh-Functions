b652d2b9bac820e4d685ac097f1143a134fbcf31
Tyler <68524461+TySP-Dev@users.noreply.github.com>
2025-08-17 15:14:37 -1000

    Delete functions directory

--- functions/weather.zsh
+++
@@ -1,8 +0,0 @@
-## Name: weather
-## Desc: Show current weather for a location (defaults to auto-detect)
-## Usage: weather [location]
-## Requires: curl
-function weather() {
-  local location="${1:-}"
-  curl -s "wttr.in/${location}?format=3"
-}
